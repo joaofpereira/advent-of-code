0ca8d57a71e91a7c167fa2b7bb0de70cb19fc080
João Pereira <jpereira@semasio.com>
2019-12-05 20:16:34 +0000

    Added Day 3

--- 2019/03/03.py
+++ 2019/03/03.py
@@ -5,11 +5,12 @@ def read_input():
 	with open('./input.txt', 'r') as input:
 		return [i.split(',') for i in input.readlines()]
 
-def add_coordinate_to_collection(collection, coordinate):
-	if coordinate in collection.keys():
-		collection[coordinate] = collection[coordinate] + 1
-	else:
-		collection[coordinate] = 1
+def merge_wires_path(collection, wire_collection):
+	for wire_pair in wire_collection:
+		if wire_pair in collection:
+			collection[wire_pair] = collection[wire_pair] + 1
+		else:
+			collection[wire_pair] = 1
 
 def calculate_manhattan_distance(initial_x, initial_y, final_x, final_y):
 	 return abs(initial_x - final_x) + abs(initial_y - final_y)
@@ -22,28 +23,30 @@ def exercise_1(wires):
 	current_x = 0
 	current_y = 0
 	for wire in wires:
+		wire_coordinate_collection = dict()
+
 		for i in wire:
 			if i[0] == 'U':
 				for i in range(0, int(i[1:].strip())):
 					current_y += 1
-					add_coordinate_to_collection(coordinates_collection, (current_x, current_y))
+					wire_coordinate_collection[(current_x, current_y)] = 1
 			elif i[0] == 'D':
 				for i in range(0, int(i[1:].strip())):
 					current_y -= 1
-					add_coordinate_to_collection(coordinates_collection, (current_x, current_y))
+					wire_coordinate_collection[(current_x, current_y)] = 1
 			elif i[0] == 'L':
 				for i in range(0, int(i[1:].strip())):
 					current_x -= 1
-					add_coordinate_to_collection(coordinates_collection, (current_x, current_y))
+					wire_coordinate_collection[(current_x, current_y)] = 1
 			elif i[0] == 'R':
 				for i in range(0, int(i[1:].strip())):
 					current_x += 1
-					add_coordinate_to_collection(coordinates_collection, (current_x, current_y))
+					wire_coordinate_collection[(current_x, current_y)] = 1
 
+		merge_wires_path(coordinates_collection, wire_coordinate_collection)
 		current_x = 0
 		current_y = 0
 	
-	min_euclidean_distance = sys.maxsize
 	min_manhattan_distance = sys.maxsize
 
 	closest_coordinate = None
