2e7f8edcf1e5a38ad302dbfef279f134e585a8dc
joaofpereira <pereiraffjoao1993@gmail.com>
2021-01-02 22:58:21 +0000

    Added exercise 3

--- 2020/advent-of-code-2020.py
+++ 2020/advent-of-code-2020.py
@@ -1,3 +1,5 @@
+import math
+
 def read_file(filename):
     with open(filename, encoding='utf-8') as file:
         return file.readlines()
@@ -64,11 +66,73 @@ def exercise_2_2():
             count = count + 1
     return count
 
+def generate_maze(original_maze, n):
+    returning_maze = []
+    for i in range(0, len(original_maze)):
+        returning_maze.append(original_maze[i] * n)
+    return returning_maze
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def do_step(self):
+        self.x = self.x + 3
+        self.y = self.y + 1
+    
+    def do_step(self, p):
+        self.x = self.x + p.x
+        self.y = self.y + p.y
+
+def exercise_3_1():
+    original_maze = [x.strip()for x in read_file("input-3.txt")]
+    p = Point(0,0)
+    increment = 1
+    trees = 0
+    maze = generate_maze(original_maze, increment)
+    while p.y < len(original_maze):
+        if p.x + 3 > len(maze[p.y]):
+            increment = increment + 1
+            maze = generate_maze(original_maze, increment)
+        if maze[p.y][p.x] == '#':
+            trees = trees + 1
+        p.do_step()
+    return trees
+
+def get_prod(l):
+    result = 1
+    for entry in l:
+        result = result * entry
+    return result
+
+def exercise_3_2():
+    original_maze = [x.strip()for x in read_file("input-3.txt")]
+    increments = [Point(1,1), Point(3,1), Point(5,1), Point(7,1), Point(1,2)]
+    results = []
+    for i in range(0, len(increments)):
+        p = Point(0,0)
+        maze_multiplier = 1
+        trees = 0
+        maze = generate_maze(original_maze, maze_multiplier)
+        while p.y < len(original_maze):
+            if p.x + increments[i].x > len(maze[p.y]):
+                maze_multiplier = maze_multiplier + 1
+                maze = generate_maze(original_maze, maze_multiplier)
+            if maze[p.y][p.x] == '#':
+                trees = trees + 1
+            p.do_step(increments[i])
+        results.append(trees)
+    return get_prod(results)
+
+
 def main():
     #print(exercise_1_1())
     #print(exercise_1_2())
     #print(exercise_2_1())
-    print(exercise_2_2())
+    #print(exercise_2_2())
+    #print(exercise_3_1())
+    print(exercise_3_2())
 
 if __name__ == "__main__":
     main()
